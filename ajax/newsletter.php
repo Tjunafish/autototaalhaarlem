2344beb2dc9900a976f0e2b851dd36a62478be92
Ricardo van Tongeren <ricardovantongeren@gmail.com>
2015-01-16 12:49:39 +0100

    newsletter and url fix php/config.php
    Signed-off-by: Ricardo van Tongeren <ricardovantongeren@gmail.com>

--- ajax/newsletter.php
+++ ajax/newsletter.php
@@ -7,23 +7,23 @@ require dirname(__FILE__).'/../php/config.php';
 $error = array();
 
 $checks 	  	= array();
-$checks[name] 	= array($_POST[name],	'empty',		'Je naam');
-$checks[email]	= array($_POST[email],	'empty email');
+$checks['name'] 	= array($_POST['name'],	'empty',		'Je naam');
+$checks['email']	= array($_POST['email'],	'empty email');
 
 $error 			= core::validate($checks);
 
 $return 		= $error;
-$return[status] = 'failed';
+$return['status'] = 'failed';
 
 if(count($error) == 0){
 
-	if(!sql::exists("newsletter",array("email"=>$_POST[email])))
-		sql::insert("newsletter",array("name" 		 => $_POST[name],
-									   "email"		 => $_POST[email],
+	if(!sql::exists("newsletter",array("email"=>$_POST['email'])))
+		sql::insert("newsletter",array("name" 		 => $_POST['name'],
+									   "email"		 => $_POST['email'],
 									   "signup_date" => date('Y-m-d H:i:s')));
 
-	$return[status] 	= 'success';
-	$return[returnText] = '<h3 class="return">U bent succesvol ingeschreven voor onze nieuwsbrief.</h3>';
+	$return['status'] 	= 'success';
+	$return['returnText'] = '<h3 class="return">U bent succesvol ingeschreven voor onze nieuwsbrief.</h3>';
 
 }
 
