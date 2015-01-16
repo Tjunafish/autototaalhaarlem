27b7c2e469349ce9e561bcac508661047dabb10f
root <root@vps001.tjuna.com>
2015-01-16 13:00:25 +0100

    fix url problems

--- php/config.php
+++ php/config.php
@@ -23,7 +23,11 @@ require_once 'social/_social.php';
 // General settings
 // ----------------
  
-define('ROOT',		  		'http://www.autototaalhaarlem.nl/');
+if (isset($_SERVER['HTTP_HOST'])) {
+	define('ROOT', $_SERVER['HTTP_HOST']);
+} else {
+	define('ROOT',		  		'http://www.autototaalhaarlem.nl/');
+}
 define('COOKIE_DOMAIN',		'/');
 define('LUXUS',        		$_SERVER['REMOTE_ADDR'] == '90.145.41.202');
 
